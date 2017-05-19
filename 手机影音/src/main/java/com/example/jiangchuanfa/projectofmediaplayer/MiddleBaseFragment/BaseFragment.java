697ed17f8b1f63a5cc832632ba1f2058af977d58
JCrest <1554687045@qq.com>
2017-05-19 19:56:22 +0800

    创建抽象基类Base Fragment，因为在Radio Group中几个控件具有一定的相似性，创建一个基类便于更好的管理！

---
+++ 手机影音/src/main/java/com/example/jiangchuanfa/projectofmediaplayer/MiddleBaseFragment/BaseFragment.java
@@ -0,0 +1,58 @@
+package com.example.jiangchuanfa.projectofmediaplayer.MiddleBaseFragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by crest on 2017/5/19.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private Context context;
+    /**
+     * 当Fragment被创建的时候回调
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();//MainActiviry
+    }
+
+    /**
+     * 当创建视图的时候回调
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+
+    }
+    /**
+     * 由子类实现的抽象方法，写不同的布局，不同的效果
+     * @return
+     */
+    public abstract View initView();//抽象的方法只能由public来修饰
+
+    /**
+     * 当依附的Activity被创建的时候回调
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();//在得到视图的基础上，设置数据
+    }
+
+    private void initData() {
+    }
+}
