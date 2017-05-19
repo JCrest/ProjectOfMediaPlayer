dca49895b1b3925573b00f13177850aa8e3166ec
JCrest <1554687045@qq.com>
2017-05-19 18:47:10 +0800

    由WelcomeActivity 进入MainActivity！

---
+++ 手机影音/src/main/java/com/example/jiangchuanfa/projectofmediaplayer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jiangchuanfa.projectofmediaplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by crest on 2017/5/19.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
--- 手机影音/src/main/java/com/example/jiangchuanfa/projectofmediaplayer/WelcomeActivity.java
+++ 手机影音/src/main/java/com/example/jiangchuanfa/projectofmediaplayer/WelcomeActivity.java
@@ -1,13 +1,44 @@
 package com.example.jiangchuanfa.projectofmediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 5000);
+
+
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        //关闭当前页面
+        finish();
+    }
+//只要屏幕触摸就直接进入主页面
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 }
