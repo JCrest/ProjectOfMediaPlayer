7174b174e2de863302d429b64de07e98d48b676d
JCrest <1554687045@qq.com>
2017-05-22 23:26:04 +0800

    联网获取数据并播放（xUtils）

---
+++ 手机影音/src/main/java/com/example/jiangchuanfa/projectofmediaplayer/app/MyApplication.java
@@ -0,0 +1,24 @@
+package com.example.jiangchuanfa.projectofmediaplayer.app;
+
+
+import android.app.Application;
+
+import org.xutils.BuildConfig;
+import org.xutils.x;
+
+/**
+ * Created by crest on 2017/5/22.
+ *
+ *
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG);
+
+
+    }
+}
